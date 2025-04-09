64dd571aeffa4e75b974896e2a092f81d962e499
Vasty Muaku <vasty@wethinkcode.co.za>
2025-04-09 14:14:50 +0200

    initialised fastapi simple greet endpoint for testing connection with react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
+  fetch('http://localhost:8000/')
+    .then(response => response.json())
+    .then(data => console.log(data.message))
+    .catch(error => console.error('Error fetching tasks:', error));
+
+return (
+  <>
     <h1>Flogger</h1>
-    </>
-  )
+  </>
+)
 }
 
 export default App
