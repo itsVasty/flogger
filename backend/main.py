d37476d29ed820996738ae4e5bd9d83fd381e81d
Vasty Muaku <vasty@wethinkcode.co.za>
2025-04-10 21:26:46 +0200

    created post basemodel and associated endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from models import Post
 
 app = FastAPI()
 
@@ -16,3 +18,12 @@ app.add_middleware(
 @app.get('/')
 async def greet():
     return {"message": "Hello, World"}
+
+@app.get('/post/{post_id}')
+async def get_post(post_id):
+    return {post_id : f"Post {post_id}"}
+
+@app.post('/post/')
+async def set_post(post : Post):
+    return post
+
