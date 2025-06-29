011f358787089d63bc4d59685c09870dff327ae0
Vasty Muaku <vasty@wethinkcode.co.za>
2025-06-29 12:46:55 +0200

    added comment form

--- frontend/src/components/utils/Post.tsx
+++ frontend/src/components/utils/Post.tsx
@@ -2,6 +2,31 @@ import { useState } from 'react';
 import { BlogPost, Comment } from '../../models.tsx';
 import '../styles.css'
 
+const CommentForm = ({ post, submitComment }: { post: BlogPost, submitComment: (post: BlogPost, comment_text: string) => void }) => {
+    const [comment_text, setCommentText] = useState('');
+
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            if (comment_text.trim() !== '') {
+                submitComment(post, comment_text);
+                setCommentText('');
+            } else {
+                alert('Comment cannot be empty');
+            }
+        }}>  
+            <label htmlFor='comment-text-editor'>Comment:</label>
+            <input
+                type='text'
+                id='comment-text-editor'
+                value={comment_text}
+                onChange={(e) => setCommentText(e.target.value)}
+            />
+            <input id='submit' type='submit' value='submit'></input>
+        </form>
+    )
+}
+
 const Post = ({ post }: { post: BlogPost }) => {
     const [post_, setPost] = useState(post)
 
@@ -18,6 +43,30 @@ const Post = ({ post }: { post: BlogPost }) => {
             .then(post => setPost(post))
             .catch(err => console.log(err));
     }
+
+    const submitComment = (post: BlogPost, comment_text: string) => {
+        const comment: Comment = {
+            id: `comment_${post.comments ? post.comments.length + 1 : 1}`,
+            owner: { id: 'comment_1', name: 'Anonymous' },
+            content: comment_text,
+            digs: 0
+        }
+
+        post.comments = post.comments || [];
+        post.comments.push(comment);
+
+        console.log(post)
+
+        fetch(`http://localhost:8000/posts/${post.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(post),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(res => res.json())
+            .then(post => setPost(post))
+            .catch(err => console.log(err));
+    }
+
     return (
         <>
             <div>
@@ -34,10 +83,11 @@ const Post = ({ post }: { post: BlogPost }) => {
                 Digs: {post_.digs}
             </div>
             <span>Comments:</span>
+            <CommentForm post={post_} submitComment={submitComment} />
             <div>
                 {post_.comments ? (
                     post_.comments.map((comment: Comment) => (
-                        <>
+                        <div key={comment.id}>
                             <h6>{comment.owner.name}</h6>
                             <p>{comment.content}</p>
                             <div
@@ -46,7 +96,7 @@ const Post = ({ post }: { post: BlogPost }) => {
                                     dig(post_)
                                 }}
                             >Digs: {comment.digs}</div>
-                        </>
+                        </div>
                     ))
                 ) : (<></>)}
             </div>
